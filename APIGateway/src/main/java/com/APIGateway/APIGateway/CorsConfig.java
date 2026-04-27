ccb38580bc022c63c460b97ff392fbd1fb9eb904
Menna hesham <mh8391802@gmail.com>
2026-04-27 22:24:58 +0300

    ‘Update cors

--- APIGateway/src/main/java/com/APIGateway/APIGateway/CorsConfig.java
+++ APIGateway/src/main/java/com/APIGateway/APIGateway/CorsConfig.java
@@ -1,24 +1,25 @@
-package com.APIGateway.APIGateway;
+// package com.APIGateway.APIGateway;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.cors.CorsConfiguration;
+// import org.springframework.web.cors.reactive.CorsWebFilter;
+// import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
-@Configuration
-public class CorsConfig {
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.addAllowedOrigin("http://localhost:5173"); // بورت الفرونت
-        corsConfig.addAllowedMethod("*");
-        corsConfig.addAllowedHeader("*");
-        corsConfig.setAllowCredentials(true);
+// @Configuration
+// public class CorsConfig {
+// @Bean
+// public CorsWebFilter corsWebFilter() {
+// CorsConfiguration corsConfig = new CorsConfiguration();
+// corsConfig.addAllowedOrigin("http://localhost:5173"); // بورت الفرونت
+// corsConfig.addAllowedMethod("*");
+// corsConfig.addAllowedHeader("*");
+// corsConfig.setAllowCredentials(true);
 
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
+// UrlBasedCorsConfigurationSource source = new
+// UrlBasedCorsConfigurationSource();
+// source.registerCorsConfiguration("/**", corsConfig);
 
-        return new CorsWebFilter(source);
-    }
-}
+// return new CorsWebFilter(source);
+// }
+// }
